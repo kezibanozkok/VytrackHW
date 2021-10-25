06ff411c9284eda70340717a18e3d8233b06b4e1
kezibanozkok <kezibanozkok@gmail.com>
2021-10-25 22:01:31 +0300

    all tests completed

--- src/test/java/com/cydeo/pages/CalendarEventsPage.java
+++ src/test/java/com/cydeo/pages/CalendarEventsPage.java
@@ -4,6 +4,8 @@ import com.cydeo.base.BasePage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class CalendarEventsPage extends BasePage {
 
     @FindBy(xpath = "//div[contains(text(),'Options')]")
@@ -13,4 +15,24 @@ public class CalendarEventsPage extends BasePage {
     @FindBy(xpath = "//input[@type= 'number']")
     public WebElement pageNumber;
 
+    @FindBy(css = "div.btn-group > button")
+    public WebElement viewPerPage;
+
+    @FindBy(xpath = "//label[@class='dib'][contains(text(), 'Total of ')]")
+    public WebElement numOfRecords;
+
+    @FindBy(xpath = "//table/tbody/tr")
+    public List<WebElement> tableRows;
+
+    @FindBy(css = ".btn.btn-default.btn-small.dropdown-toggle>input")
+    public WebElement selectAllCheckbox;
+
+    @FindBy(css = ".select-row-cell.renderable.grid-cell.grid-body-cell.grid-body-cell-massAction>input")
+    public List<WebElement> tableCheckboxes;
+
+    @FindBy(xpath = "//*[. = 'Testers Meeting'] ")
+    public WebElement testersMeeting;
+
+    @FindBy(xpath = "//div[@class='responsive-block']/div")
+    public List<WebElement> testersMeetingInfo;
 }
--- src/test/java/com/cydeo/tests/Test1.java
+++ src/test/java/com/cydeo/tests/Test1.java
@@ -4,8 +4,12 @@ import com.cydeo.base.TestBase;
 import com.cydeo.pages.CalendarEventsPage;
 import com.cydeo.pages.DashboardPage;
 import com.cydeo.pages.LoginPage;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Test1 extends TestBase {
 
@@ -33,5 +37,77 @@ public class Test1 extends TestBase {
         String pageValue = calendarEventsPage.pageNumber.getAttribute("value");
 
         Assert.assertEquals(pageValue, "1", "verify page number equals to 1");
+    }
+
+    @Test
+    public void test3() {
+        loginPage.loginAsStoreManager();
+
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        dashboardPage.waitUntilLoaderScreenDisappear();
+
+        String viewPerPageValue = calendarEventsPage.viewPerPage.getAttribute("innerHTML").trim().substring(0,2);
+        //System.out.println(viewPerPageValue);
+
+        Assert.assertEquals(viewPerPageValue, "25", "verify view per page is equal to 25");
+
+    }
+
+    @Test
+    public void test4() {
+        loginPage.loginAsStoreManager();
+
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        dashboardPage.waitUntilLoaderScreenDisappear();
+
+        String[] records = calendarEventsPage.numOfRecords.getText().split(" ");
+
+        int actualRow = calendarEventsPage.tableRows.size();
+        int expectedRow = Integer.parseInt(records[records.length-2]);
+
+        Assert.assertEquals(actualRow, expectedRow, "verify row size equals to record");
+    }
+
+    @Test
+    public void test5() {
+        loginPage.loginAsStoreManager();
+
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        calendarEventsPage.selectAllCheckbox.click();
+
+        for (WebElement ch: calendarEventsPage.tableCheckboxes) {
+            Assert.assertTrue(ch.isSelected());
+        }
+    }
+
+    @Test
+    public void test6() {
+        loginPage.loginAsStoreManager();
+
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        calendarEventsPage.testersMeeting.click();
+
+        Map<String, String> expectedTable = new LinkedHashMap<>();
+        expectedTable.put("Title","Testers Meeting");
+        expectedTable.put("Description","This is a a weekly testers meeting");
+        expectedTable.put("Start","Nov 27, 2019, 9:30 AM");
+        expectedTable.put("End","Nov 27, 2019, 10:30 AM");
+        expectedTable.put("All-Day Event","No");
+        expectedTable.put("Organizer","John Doe");
+        expectedTable.put("Recurrence","Weekly every 1 week on Wednesday");
+        expectedTable.put("Call Via Hangout","No");
+
+        Map<String, String> actualTable = new LinkedHashMap<>();
+
+        for (WebElement info : calendarEventsPage.testersMeetingInfo) {
+            String[] keyValue = info.getText().trim().split("\n");
+            //    System.out.println( keyValue[0] + " : " + keyValue[1]);
+            actualTable.put(keyValue[0], keyValue[1]);
+        }
+        Assert.assertEquals(actualTable,expectedTable);
     }
 }
